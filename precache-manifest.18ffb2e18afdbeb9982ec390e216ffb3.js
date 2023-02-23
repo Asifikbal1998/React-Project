a030cd28837664417129cbc6c53947245619cade
Asif Ikbal <asifikbal8081@gmail.com>
2023-02-23 19:00:02 +0530

    Updates

---
+++ precache-manifest.18ffb2e18afdbeb9982ec390e216ffb3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcca3f11d217b630b8c6",
+    "url": "/React-Project/static/css/main.a926ce26.chunk.css"
+  },
+  {
+    "revision": "bcca3f11d217b630b8c6",
+    "url": "/React-Project/static/js/main.bcca3f11.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/React-Project/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "ec8eb264682cadc59bcc",
+    "url": "/React-Project/static/js/2.ec8eb264.chunk.js"
+  },
+  {
+    "revision": "a808928434d9bc35e00b",
+    "url": "/React-Project/static/js/runtime~main.a8089284.js"
+  },
+  {
+    "revision": "bb4a70074844569fe6f9629ee9ce2da1",
+    "url": "/React-Project/static/media/img1.bb4a7007.jpg"
+  },
+  {
+    "revision": "8678e17142d5caceb81462ca5edfd2ae",
+    "url": "/React-Project/static/media/img2.8678e171.jpg"
+  },
+  {
+    "revision": "76ede45d63cf39dea213d02f2a034c8a",
+    "url": "/React-Project/static/media/img3.76ede45d.jpg"
+  },
+  {
+    "revision": "788a3f003d308013d1c28f550073d0f4",
+    "url": "/React-Project/static/media/img4.788a3f00.jpg"
+  },
+  {
+    "revision": "a3b59721f25fbe61524a08a50458ffb0",
+    "url": "/React-Project/static/media/img5.a3b59721.jpg"
+  },
+  {
+    "revision": "8f9c1718ba8c7f31c16e7fcd49142396",
+    "url": "/React-Project/static/media/img6.8f9c1718.jpg"
+  },
+  {
+    "revision": "c8d8093ab62e3755b22327fd4aea422f",
+    "url": "/React-Project/index.html"
+  }
+];
